c2ef3fa4273786f4831ef0a4806937094ced8369
Andrea Mannocci <andremann@libero.it>
2023-09-20 17:14:36 +0200

    committing cookiecutter template

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='20-year retrospective analysis of IRCDL',
+    author='Andrea Mannocci',
+    license='MIT',
+)
