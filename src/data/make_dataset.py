9de532a5c1bc6dfe377b65627ed5f7d528b36cdb
Andrea Mannocci <andremann@libero.it>
2023-09-22 12:02:58 +0200

    first notebook

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -4,6 +4,13 @@ import logging
 from pathlib import Path
 from dotenv import find_dotenv, load_dotenv
 
+import os
+import json, csv
+from urllib.request import urlretrieve
+
+
+DBLP = 'https://dblp.org/search/publ/api?q=stream%3Astreams%2Fconf%2Fircdl%3A&h=1000&format=json'
+
 
 @click.command()
 @click.argument('input_filepath', type=click.Path(exists=True))
@@ -14,6 +21,37 @@ def main(input_filepath, output_filepath):
     """
     logger = logging.getLogger(__name__)
     logger.info('making final data set from raw data')
+
+    dblp_path = os.path.join(input_filepath, 'ircdl_dblp.json')
+    csv_path = os.path.join(output_filepath, 'ircdl_dblp.csv')
+    if not os.path.exists(dblp_path):
+        urlretrieve(DBLP, dblp_path)
+
+    with open(dblp_path) as dblp_file, open(csv_path, 'w') as csv_file:
+        dblp = json.load(dblp_file)
+
+        writer = csv.writer(csv_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        writer.writerow(['doi', 'url', 'ee', 'year', 'authors', 'title', 'pages', 'length', 'type', 'access'])
+        for hit in dblp['result']['hits']['hit']:
+            paper = hit['info']
+            
+            if 'authors' in paper:
+                # authors = ', '.join([a['text'] for a in paper['authors']['author']]) if isinstance(paper['authors']['author'], list) else paper['authors']['author']['text']
+                authors = [a['text'] for a in paper['authors']['author']] if isinstance(paper['authors']['author'], list) else [paper['authors']['author']['text']]
+            else:
+                authors = []
+
+            doi = paper['doi'] if 'doi' in paper else None
+            access = paper['access'] if 'access' in paper else None
+            ee = paper['ee'] if 'ee' in paper else None
+            range = paper['pages'] if 'pages' in paper else None
+            if range is not None:
+                pages = range.split('-')
+                length = int(pages[1]) - int(pages[0])
+            else:
+                length = None
+            
+            writer.writerow([doi, paper['url'], ee, paper['year'], authors, paper['title'], range, length, paper['type'], access])
 
 
 if __name__ == '__main__':
